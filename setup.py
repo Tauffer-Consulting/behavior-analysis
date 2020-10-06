2286354d733e1ff947ba9bbc410aa51aaa85340b
luiztauffer <luiz_tauffer@hotmail.com>
2020-10-06 11:50:36 +0200

    umap dependency

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'pandas', 'numpy', 'Flask', 'Flask-SQLAlchemy',
-        'dash', 'dash_bootstrap_components', 'dash-canvas', 'pandas-profiling'
+        'dash', 'dash_bootstrap_components', 'dash-canvas', 'pandas-profiling',
+        'umap-learn'
     ]
 )
